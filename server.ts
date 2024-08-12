2bfc0aa7430ea959155bf2007f4cb733006daa29
eden.zhang <eden.zhang@bytetime.io>
2024-08-12 15:26:21 +0800

    test

---
+++ server.ts
@@ -0,0 +1,27 @@
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const path = require("path");
+const port = 8994;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const keyPath = path.join(__dirname, "localhost+2-key.pem");
+const certPath = path.join(__dirname, "localhost+2.pem");
+
+const httpsOptions = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req: any, res: any) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err: any) => {
+    if (err) throw err;
+    console.log("ready - started server on url: https://localhost:" + port);
+  });
+});
