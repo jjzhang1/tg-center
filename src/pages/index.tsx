3cbd18c86e5a4d0cd8b6004418642b2a1c432ede
eden.zhang <eden.zhang@bytetime.io>
2024-08-12 15:54:59 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,8 @@ import Image from "next/image";
 export default function Index(props: any) {
   return (
     <div className="index_container">
-      {/* <img src="/assets/images/home.svg" alt="" /> */}
       <Image
-        src="/assets/images/home.svg"
-        alt=""
-        layout="responsive"
-        placeholder="blur"
-        blurDataURL="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-        width={1}
-        height={1}
-      />
-      <Image
-        src="/assets/images/home.svg"
-        alt=""
-        layout="responsive"
-        placeholder="blur"
-        blurDataURL="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-        width={1}
-        height={1}
-      />
-      <Image
-        src="/assets/images/home.svg"
+        src="/assets/images/home.png"
         alt=""
         layout="responsive"
         placeholder="blur"
